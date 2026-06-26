afcb0249c1e4edc730110e3d5c3d599f82853d13
parteek1809 <pt07276@gmail.com>
2026-06-26 23:56:58 +0530

    Update 0733-flood-fill.java

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,29 +1,29 @@
-// class Solution {
-//     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-//         int m = image.length;
-//         int n = image[0].length;
-//         Queue<int[]> q = new LinkedList<>();
-//         int original = image[sr][sc];
-//         if(original == color) return image;
-//         q.add(new int[] {sr,sc});
-//         while(!q.isEmpty()){
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int m = image.length;
+        int n = image[0].length;
+        Queue<int[]> q = new LinkedList<>();
+        int original = image[sr][sc];
+        if(original == color) return image;
+        q.add(new int[] {sr,sc});
+        while(!q.isEmpty()){
 
-//             int curr[] = q.remove();
-//             int r = curr[0];
-//             int c = curr[1];
-//             image[r][c] = color;
-//             int neighbour[][] = { {r+1,c}, {r-1,c}, {r,c+1}, {r,c-1} };
-//             for(int neigh[] : neighbour){
-//                 int nr = neigh[0];
-//                 int nc = neigh[1];
-//                 if(nr<0 || nc<0 || nr>=m || nc>=n || image[nr][nc] != original) continue;
-//                 image[nr][nc] = color;
-//                 q.add(new int[] {nr,nc});
-//             }
-//         }
-//         return image;
-//     }
-// }
+            int curr[] = q.remove();
+            int r = curr[0];
+            int c = curr[1];
+            image[r][c] = color;
+            int neighbour[][] = { {r+1,c}, {r-1,c}, {r,c+1}, {r,c-1} };
+            for(int neigh[] : neighbour){
+                int nr = neigh[0];
+                int nc = neigh[1];
+                if(nr<0 || nc<0 || nr>=m || nc>=n || image[nr][nc] != original) continue;
+                image[nr][nc] = color;
+                q.add(new int[] {nr,nc});
+            }
+        }
+        return image;
+    }
+}
 
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
