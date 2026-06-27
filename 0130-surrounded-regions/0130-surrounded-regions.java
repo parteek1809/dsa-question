c8b448cc9284e78e84c6d79838dff39687ad842a
parteek1809 <pt07276@gmail.com>
2026-06-28 01:10:48 +0530

    Time: 2 ms (72.78%), Space: 47.5 MB (86.1%) - LeetHub

---
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -0,0 +1,51 @@
+class Solution {
+    public void solve(char[][] board) {
+        int m = board.length;
+        int n = board[0].length;
+        boolean vis[][] = new boolean[m][n];
+        for(int i=0; i<n; i++){  //top
+            if(board[0][i] == 'O'){
+                dfs(board, 0, i, vis);
+            }
+        } 
+        for(int i=0; i<n; i++){  //bottom
+            if(board[m-1][i] == 'O'){
+                dfs(board, m-1, i, vis);
+            }
+        } 
+        for(int i=1; i<m-1; i++){   // left
+            if(board[i][0] == 'O'){
+                dfs(board, i, 0, vis);
+            }
+        } 
+        for(int i=1; i<m-1; i++){
+            if(board[i][n-1] == 'O'){
+                dfs(board, i, n-1, vis);
+            }
+        }
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(board[i][j] == '#'){
+                    board[i][j] = 'O';
+                }
+                else{
+                    board[i][j] = 'X';
+                }
+            }
+        } 
+    }
+    public void dfs(char[][] board, int r, int c, boolean[][] vis){
+        // vis[r][c] = true;
+        int m = board.length;
+        int n = board[0].length;
+        if(r<0 || c<0 || r>=m || c>=n || board[r][c] != 'O' || vis[r][c] == true) return;
+        board[r][c] = '#';
+        vis[r][c] = true;
+        int[][] neighbour = { {r+1,c}, {r-1,c}, {r,c+1}, {r,c-1}};
+        for(int neigh[] : neighbour){ 
+            int nr = neigh[0];
+            int nc = neigh[1];
+            dfs(board, nr, nc, vis);
+        }
+    }
+}
