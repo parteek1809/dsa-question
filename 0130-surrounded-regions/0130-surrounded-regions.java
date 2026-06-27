ea996108e633116a1a846afb9019d6f54f298f9a
parteek1809 <pt07276@gmail.com>
2026-06-28 01:16:16 +0530

    Time: 2 ms (72.78%), Space: 47.5 MB (86.1%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -2,25 +2,25 @@ class Solution {
     public void solve(char[][] board) {
         int m = board.length;
         int n = board[0].length;
-        boolean vis[][] = new boolean[m][n];
+        // boolean vis[][] = new boolean[m][n];
         for(int i=0; i<n; i++){  //top
             if(board[0][i] == 'O'){
-                dfs(board, 0, i, vis);
+                dfs(board, 0, i);
             }
         } 
         for(int i=0; i<n; i++){  //bottom
             if(board[m-1][i] == 'O'){
-                dfs(board, m-1, i, vis);
+                dfs(board, m-1, i);
             }
         } 
         for(int i=1; i<m-1; i++){   // left
             if(board[i][0] == 'O'){
-                dfs(board, i, 0, vis);
+                dfs(board, i, 0);
             }
         } 
         for(int i=1; i<m-1; i++){
             if(board[i][n-1] == 'O'){
-                dfs(board, i, n-1, vis);
+                dfs(board, i, n-1);
             }
         }
         for(int i=0; i<m; i++){
@@ -34,18 +34,18 @@ class Solution {
             }
         } 
     }
-    public void dfs(char[][] board, int r, int c, boolean[][] vis){
+    public void dfs(char[][] board, int r, int c ){
         // vis[r][c] = true;
         int m = board.length;
         int n = board[0].length;
-        if(r<0 || c<0 || r>=m || c>=n || board[r][c] != 'O' || vis[r][c] == true) return;
+        if(r<0 || c<0 || r>=m || c>=n || board[r][c] != 'O' ) return;
         board[r][c] = '#';
-        vis[r][c] = true;
+        // vis[r][c] = true;
         int[][] neighbour = { {r+1,c}, {r-1,c}, {r,c+1}, {r,c-1}};
         for(int neigh[] : neighbour){ 
             int nr = neigh[0];
             int nc = neigh[1];
-            dfs(board, nr, nc, vis);
+            dfs(board, nr, nc);
         }
     }
 }
