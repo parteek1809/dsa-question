71ac12fc4f5640410eec620a8a12ff25f0519a48
parteek1809 <pt07276@gmail.com>
2026-06-28 18:33:52 +0530

    Update 0785-is-graph-bipartite.java

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -1,32 +1,32 @@
-// class Solution {
-//     public boolean isBipartite(int[][] graph) {
-//         int[] col = new int[graph.length];
-//         // for(int i=0; i<graph.length; i++){
-//         //     col[i] = -1;
-//         // }
-//         Arrays.fill(col,-1);
-//         for(int i=0;i<graph.length;i++){
-//             if(col[i] != -1) continue;
-//             Queue<Integer> q = new LinkedList<>();
-//             q.add(i);
-//             col[i] = 1;
-//             while(!q.isEmpty()){
-//                 int curr = q.remove();
-//                 for(int j=0; j<graph[curr].length; j++){
-//                     int neigh = graph[curr][j];
-//                     if(col[neigh] == -1){
-//                         col[neigh] = 1 - col[curr];
-//                         q.add(neigh);
-//                     }
-//                     else if (col[neigh] == col[curr]){
-//                         return false;
-//                     }
-//                 }
-//             }
-//         }
-//         return true;
-//     }
-// }
+class Solution {
+    public boolean isBipartite(int[][] graph) {
+        int[] col = new int[graph.length];
+        // for(int i=0; i<graph.length; i++){
+        //     col[i] = -1;
+        // }
+        Arrays.fill(col,-1);
+        for(int i=0;i<graph.length;i++){
+            if(col[i] != -1) continue;
+            Queue<Integer> q = new LinkedList<>();
+            q.add(i);
+            col[i] = 1;
+            while(!q.isEmpty()){
+                int curr = q.remove();
+                for(int j=0; j<graph[curr].length; j++){
+                    int neigh = graph[curr][j];
+                    if(col[neigh] == -1){
+                        col[neigh] = 1 - col[curr];
+                        q.add(neigh);
+                    }
+                    else if (col[neigh] == col[curr]){
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
 
 class Solution {
 
