ce3fe7f4b6a9605541134ecc2b5eb7ac1da7e35e
parteek1809 <pt07276@gmail.com>
2026-06-30 00:39:31 +0530

    Time: 3 ms (99.77%), Space: 47 MB (77.12%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -37,3 +37,53 @@ class Solution {
         return false;
     }
 }
+
+// Time Complexity - O(N + E)
+// Space Complexity - O(2N)
+
+// class Solution {
+//     public boolean canFinish(int numCourses, int[][] prerequisites) {
+//         int N = prerequisites.length;
+//         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+//         for(int i = 0; i<numCourses; i++) {
+//             ArrayList<Integer> sub = new ArrayList<>();
+//             adj.add(sub);
+//         }
+
+//         for(int i = 0; i<N; i++) {
+//             adj.get(prerequisites[i][1]).add(prerequisites[i][0]);
+//         }
+
+//         int indegree[] = new int[numCourses];
+//         for(int i = 0; i<numCourses; i++) {
+//             for(int it : adj.get(i)) {
+//                 indegree[it]++;
+//             }
+//         }
+//         Queue<Integer> q = new LinkedList<>();
+//         for(int i = 0; i<numCourses; i++) {
+//             if(indegree[i] == 0) {
+//                 q.offer(i);
+//             }
+//         }
+
+//         List<Integer> ans = new ArrayList<>();
+
+//         while(!q.isEmpty()) {
+//             int node = q.poll();
+//             ans.add(node);
+
+//             for(int it: adj.get(node)) {
+//                 indegree[it]--;
+//                 if(indegree[it] == 0) {
+//                     q.offer(it);
+//                 }
+//             }
+//         }
+
+//         if(ans.size() == numCourses) {
+//             return true;
+//         }
+//         return false;
+//     }
+// }
