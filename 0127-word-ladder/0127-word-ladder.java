44310e4f78bca05e15caf50512f1688ae5b5200c
parteek1809 <pt07276@gmail.com>
2026-07-02 19:27:15 +0530

    Time: 68 ms (76.81%), Space: 47.9 MB (66.59%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -1,39 +1,86 @@
-class Pair{
-    String first;
-    int second;
-    public Pair(String first, int second){
-        this.first = first;
-        this.second = second;
-    }
-}
-class Solution {
+// class Pair{
+//     String first;
+//     int second;
+//     public Pair(String first, int second){
+//         this.first = first;
+//         this.second = second;
+//     }
+// }
+// class Solution {
 
+//     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+//         HashSet<String> hs = new HashSet<>();
+//         for(String s : wordList){
+//             hs.add(s);
+//         }
+//         if(!hs.contains(endWord)) return 0;
+//         Queue<Pair> q = new LinkedList<>();
+//         q.add(new Pair (beginWord,1));
+//         if(hs.contains(beginWord)) hs.remove(beginWord);
+//         while(!q.isEmpty()){
+//             Pair p = q.poll();
+//             String curr = p.first;
+//             int steps = p.second;
+//             if(curr.equals(endWord)) return steps;
+//             for(int i=0; i<curr.length(); i++){
+//                 for(char ch='a'; ch<='z'; ch++){
+//                     char charArray[] = curr.toCharArray();
+//                     charArray[i] = ch;
+//                     String replaceWord = new String(charArray);
+//                     if(hs.contains(replaceWord)){
+//                         hs.remove(replaceWord);
+//                         q.add(new Pair(replaceWord, steps+1));
+//                     }
+//                 }
+//             }
+//         }
+//         return 0;
+//     }
+// }
+
+class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        HashSet<String> hs = new HashSet<>();
-        for(String s : wordList){
-            hs.add(s);
+        Set<String> st = new HashSet<>(wordList);
+
+        if (!st.contains(endWord)) {
+            return 0;
         }
-        if(!hs.contains(endWord)) return 0;
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair (beginWord,1));
-        if(hs.contains(beginWord)) hs.remove(beginWord);
+
+        Queue<String> q = new LinkedList<>();
+        q.offer(beginWord);
+        st.remove(beginWord);
+        int level=1;
+
         while(!q.isEmpty()){
-            Pair p = q.poll();
-            String curr = p.first;
-            int steps = p.second;
-            if(curr.equals(endWord)) return steps;
-            for(int i=0; i<curr.length(); i++){
-                for(char ch='a'; ch<='z'; ch++){
-                    char charArray[] = curr.toCharArray();
-                    charArray[i] = ch;
-                    String replaceWord = new String(charArray);
-                    if(hs.contains(replaceWord)){
-                        hs.remove(replaceWord);
-                        q.add(new Pair(replaceWord, steps+1));
+            int size = q.size();
+            for(int i=0;i<size;i++){
+                String curr = q.poll();
+
+                if(curr.equals(endWord)){
+                    return level;
+                }
+
+                for(int j=0;j<curr.length();j++){
+                    char[] word = curr.toCharArray();
+                    char original = word[j];
+
+                    for(char c = 'a';c<='z';c++){
+                        if(c==original)continue;
+                        word[j]=c;
+                        String newWord=new String(word);
+                        if(st.contains(newWord)){
+                            q.offer(newWord);
+                            st.remove(newWord);
+                        }
+                        
                     }
                 }
             }
+            level++;
         }
+
         return 0;
+
+
     }
 }
