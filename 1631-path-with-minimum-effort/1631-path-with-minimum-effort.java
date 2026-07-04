3f393bc94a8459f924546eafc608a1a90bcc69a4
parteek1809 <pt07276@gmail.com>
2026-07-05 01:45:49 +0530

    Time: 39 ms (85.06%), Space: 48 MB (39.88%) - LeetHub

---
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int minimumEffortPath(int[][] heights) {
+        int n = heights.length;
+        int m = heights[0].length;
+        int[][] dist = new int[n][m];
+        for(int rows[] : dist ){
+            Arrays.fill(rows, Integer.MAX_VALUE);
+        }
+        int dr[] = {1,-1,0,0};
+        int dc[] = {0,0,1,-1};
+        PriorityQueue<int[]> q = new PriorityQueue<>(new Comparator<int[]>(){
+            public int compare(int first[], int[] second){
+                return first[0]-second[0];
+            }
+        });
+        dist[0][0] = 0;
+        q.add(new int[] {0,0,0});
+        while(!q.isEmpty()){
+            int curr[] = q.poll();
+            int effort = curr[0];
+            int r = curr[1];
+            int c = curr[2];
+            if(r == n-1 && c == m-1) return effort;
+            for(int i=0;i<4;i++){
+                int nr = r + dr[i];
+                int nc = c + dc[i];
+                if(nr<0 || nc<0 || nr>=n || nc>= m ) continue;
+                int edgeDistance = Math.abs(heights[r][c] - heights[nr][nc]);
+                int newEffort = Math.max(edgeDistance, effort);
+                if(newEffort < dist[nr][nc]){
+                    dist[nr][nc] = newEffort;
+                    q.add(new int[] {newEffort, nr,nc});
+                }
+            }
+        }
+        return -1;
+    }
+}
