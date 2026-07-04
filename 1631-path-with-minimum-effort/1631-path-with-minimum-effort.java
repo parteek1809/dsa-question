e9742d800929885abe5843c308968ed0e0d44074
parteek1809 <pt07276@gmail.com>
2026-07-05 01:55:06 +0530

    Time: 39 ms (85.06%), Space: 47.8 MB (72.75%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
@@ -21,11 +21,11 @@ class Solution {
             int effort = curr[0];
             int r = curr[1];
             int c = curr[2];
+            if (effort > dist[r][c]) continue;  // reduce unnecessary steps
             if(r == n-1 && c == m-1) return effort;
             for(int i=0;i<4;i++){
                 int nr = r + dr[i];
                 int nc = c + dc[i];
-                if (effort > dist[r][c]) continue;  // reduce unnecessary steps
                 if(nr<0 || nc<0 || nr>=n || nc>= m ) continue;
                 int edgeDistance = Math.abs(heights[r][c] - heights[nr][nc]);
                 int newEffort = Math.max(edgeDistance, effort);
